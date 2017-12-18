885d963c11e83d40f8a46f25f6e3d0ba37af86e1
Abraham Nieto <elmeromero.an@gmail.com>
2017-12-18 13:55:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,8 +17,10 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string === whisper(string)) {
     return "I can't hear you!"
-  }
-  else if (string === shout(string)) {
+  } else if (string === shout(string)) {
     return "YES INDEED!"
+  } else if (string === "I love you, Grandma.") {
+    return "I love you, too."
   }
+  
 }
